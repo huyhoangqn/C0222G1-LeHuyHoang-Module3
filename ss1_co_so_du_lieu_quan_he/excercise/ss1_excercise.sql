665466a4da02b71c943fa9e5acb1c3f217036efb
huyhoangqn <lehuyhoangpass1@gmail.com>
2022-05-12 14:01:23 +0700

    ss1

---
+++ ss1_co_so_du_lieu_quan_he/excercise/ss1_excercise.sql
@@ -0,0 +1,11 @@
+create database student_management;
+create table student_management.class(
+id int, 
+`name` varchar(255)
+);
+create table student_management.teacher (
+    id int
+    `name` varchar(255),
+    age int
+    country varchar(255)
+);
